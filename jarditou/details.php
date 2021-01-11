96403d4c64c83f95bd0fcdcac7cf4c0bb94ba83d
Irina Almaz <ilyamakovairina@gmail.com>
2021-01-11 11:39:41 +0100

    git push -u origin maie

---
+++ jarditou/details.php
@@ -0,0 +1,23 @@
+<?php
+include("header.php"); 
+require "connexion_bdd.php"; // Inclusion de notre bibliothèque de fonctions
+
+        $db = connexionBase(); // Appel de la fonction deconnexion
+        $pro_id = $_GET['id'];
+        var_dump($pro_id);
+        $requete = ('SELECT * FROM produits WHERE pro_id='.$pro_id);
+        
+        $result = $db->query($requete);
+      
+        // Renvoi de l'enregistrement sous forme d'un objet
+        $produit = $result->fetch(PDO::FETCH_OBJ);
+      ?>
+  </head>
+  <body> 
+    <?php echo $produit->pro_libelle; ?> référence 
+    <?php echo $produit->pro_ref; ?>
+    <?php echo $produit->pro_description; ?>
+    <?php echo $produit->pro_prix; ?>
+  </body>
+       
+</html>
